8fd6cc0bab91dc3ba7e9b8821c9950bb72b218e4
David Ruiz <dxvid.ruiz2@gmail.com>
2024-01-31 19:59:36 -0500

    password update three

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { AnyAction, configureStore } from "@reduxjs/toolkit";
+import { Tuple, UnknownAction, configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { persistReducer, persistStore } from "redux-persist";
-import thunk, { ThunkDispatch } from "redux-thunk";
+import { ThunkDispatch, thunk } from "redux-thunk";
 import rootReducer from "./rootReducer";
 
 
@@ -12,13 +12,23 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = configureStore({ reducer: persistedReducer, middleware: [thunk] });
+
+// export const store = configureStore({
+// reducer: persistedReducer, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
+
+// });
+
+export const store = configureStore({
+  reducer: persistedReducer, middleware: () => new Tuple(thunk),
+
+});
+
 export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 
-export type AppThunkDispatch = ThunkDispatch<RootState, any, AnyAction>;
+export type AppThunkDispatch = ThunkDispatch<RootState, any, UnknownAction>;
 
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
